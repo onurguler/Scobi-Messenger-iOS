aa127594d1984dd3b8d70351fb3710d9804e8de4
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-24 00:52:42 +0300

    Initial Commit

---
+++ Scobi-Messenger-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scobi-Messenger-iOS
+//
+//  Created by Onur Osman Güler on 24.07.2020.
+//  Copyright © 2020 Design X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
